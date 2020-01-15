5fb4035deceaef5983cf0bfc442b9eabe18c0df4
Boris Makaev <b.makaev@innopolis.ru>
2020-01-15 13:10:27 +0300

    add FLYWEIGHT design pattern example

---
+++ src/ru/haxor/structural/flyweight/forest/Forest.java
@@ -0,0 +1,27 @@
+package ru.haxor.structural.flyweight.forest;
+
+import ru.haxor.structural.flyweight.trees.Tree;
+import ru.haxor.structural.flyweight.trees.TreeFactory;
+import ru.haxor.structural.flyweight.trees.TreeType;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Forest extends JFrame {
+    private List<Tree> trees = new ArrayList<>();
+
+    public void plantTree(int x, int y, String name, Color color, String otherTreeData) {
+        TreeType type = TreeFactory.getTreeType(name, color, otherTreeData);
+        Tree tree = new Tree(x, y, type);
+        trees.add(tree);
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        for (Tree tree : trees) {
+            tree.draw(graphics);
+        }
+    }
+}
